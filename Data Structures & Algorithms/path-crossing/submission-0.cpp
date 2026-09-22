61e6c317e48e7a5020bead3d15e4b94989407249
Frenil0393 <frenilvaghasia393@gmail.com>
2026-09-22 17:46:14 +0530

    Add: path-crossing - submission-0

---
+++ Data Structures & Algorithms/path-crossing/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPathCrossing(string path) {
+        int x = 0, y = 0;
+        unordered_set<string> visited;
+        visited.insert("0,0");  // starting point
+
+        for (char dir : path) {
+            if (dir == 'N') y++;
+            else if (dir == 'S') y--;
+            else if (dir == 'E') x++;
+            else if (dir == 'W') x--;
+
+            string pos = to_string(x) + "," + to_string(y);
+            if (visited.count(pos)) return true;
+            visited.insert(pos);
+        }
+        return false;
+    }
+};
