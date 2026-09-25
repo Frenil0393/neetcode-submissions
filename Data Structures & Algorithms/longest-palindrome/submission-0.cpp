66e90b851e81f2802a268aab2cf682301daaf239
Frenil0393 <frenilvaghasia393@gmail.com>
2026-09-25 08:52:57 +0530

    Add: longest-palindrome - submission-0

---
+++ Data Structures & Algorithms/longest-palindrome/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        unordered_map<char, int> freq;
+        for (char c : s) {
+            freq[c]++;
+        }
+        
+        int length = 0;
+        bool hasOdd = false;
+        
+        for (auto& [ch, count] : freq) {
+            length += (count / 2) * 2;  // add even part
+            if (count % 2 == 1) {
+                hasOdd = true;
+            }
+        }
+        
+        if (hasOdd) length += 1;  // one odd goes in the center
+        return length;
+    }
+};
